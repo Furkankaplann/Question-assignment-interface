aacc45c691e7c369b18a865dd5f01b792ea911c6
Furkankaplann <fkaplanntr@gmail.com>
2023-09-21 12:31:45 +0300

    minor corrections

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,9 @@
 import "./navbar.scss";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
-import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
-import { DarkModeContext } from "../../context/darkModeContext";
-import { useContext } from "react";
-
 const Navbar = () => {
-  const { dispatch } = useContext(DarkModeContext);
 
   return (
     <div className="navbar">
+<h1>Okulistik Soru Atama Yüzü</h1>
     </div>
   );
 };
